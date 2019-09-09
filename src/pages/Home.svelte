c02bc064f96a84198d8111d9c7f83bb547a79bc9
Daniel Trusman <dtrusman@gmail.com>
2019-09-09 14:26:03 -0400

    Add svelte router

---
+++ src/pages/Home.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let name;
+</script>
+
+<h1>Welcome to svelte, {name}</h1>
--- src/shell.svelte
+++ src/shell.svelte
@@ -1,7 +1,13 @@
 <script>
   /** Imports */
+  import { Router, Route } from "svelte-routing";
   import { onMount, onDestroy } from "svelte";
   import Button from "@packages/shared/button.svelte";
+
+  import Navbar from "./components/Navbar.svelte";
+  import Home from './pages/Home.svelte';
+  import About from './pages/About.svelte';
+
 </script>
 
 <style>
@@ -22,7 +28,16 @@
 </style>
 
 <!-- Simulate a Shell Architecture -->
-<div class="shell">
-  <h1>Svelte App</h1>
-  <Button />
-</div>
+<Router>
+  <div class="shell">
+    <h1>Svelte App</h1>
+    <Navbar />
+    <Button />
+    <div class="container">
+      <Route path="/">
+        <Home name={"MyName"} />
+      </Route>
+      <Route path="/about" component={About} />
+    </div>
+  </div>
+</Router>
