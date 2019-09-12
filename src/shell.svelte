e02497d4ecf2f3ec1f8711d4a527fb5064b43412
Lucas Marinho <lucasfrotam@gmail.com>
2019-09-12 17:26:27 -0400

    add: testing the card component example

--- src/shell.svelte
+++ src/shell.svelte
@@ -3,9 +3,14 @@
   import { Router, Route } from "svelte-routing";
   import { onMount, onDestroy } from "svelte";
   import Button from "@packages/shared/button.svelte";
+  import Card from "@packages/shared/card.svelte";
 
   import Navbar from "./components/Navbar.svelte";
   import { Home, About } from "./pages";
+
+  let title;
+  let subtitle;
+  let content;
 </script>
 
 <style>
@@ -23,6 +28,14 @@
     color: aliceblue; /** TODO: remove */
     background: #363636;
   }
+  .card-section {
+    padding: 20px;
+    width: 30%;
+  }
+
+  .card-section div {
+    padding-bottom: 15px;
+  }
 </style>
 
 <!-- Simulate a Shell Architecture -->
@@ -36,6 +49,22 @@
         <Home name={'MyName'} />
       </Route>
       <Route path="/about" component={About} />
+    </div>
+    <br />
+    <div class="card-section">
+      <div>
+        Title:
+        <input bind:value={title} placeholder="input title" />
+      </div>
+      <div>
+        Subtitle:
+        <input bind:value={subtitle} placeholder="input subtitle" />
+      </div>
+      <div>
+        Content:
+        <input bind:value={content} placeholder="input content" />
+      </div>
+      <Card {title} {subtitle} {content} />
     </div>
   </div>
 </Router>
